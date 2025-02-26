62aaf1cdc1f3c0c672d804a60c60486188870914
hung.le <hung.le>
2025-02-26 16:46:03 +0700

    feat: [MYRIA-1436] test

--- configs/jest-ci.config.ts
+++ configs/jest-ci.config.ts
@@ -5,13 +5,10 @@ const config: Config = {
     moduleFileExtensions: ['ts', 'json', 'js'],
     setupFiles: ['./tests/setup/setup-jest-mock-3rds.ts'],
     testTimeout: 3000,
-    verbose: true,
     preset: 'ts-jest',
     testEnvironment: 'node',
     bail: 1,
     testRegex: '.*\\.test\\.ts$',
-    notify: true,
-    collectCoverage: true,
     coverageDirectory: './coverage',
     collectCoverageFrom: ['./src/*.{ts,js,jsx}'],
     coverageThreshold: {
